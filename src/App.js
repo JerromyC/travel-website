210f947b1a47ad7ad0b3fe8234327ce0fbcfd845
JerromyC <59486979+JerromyC@users.noreply.github.com>
2022-06-06 16:49:48 -0500

    Attempt 05: Stil....trying...

--- src/App.js
+++ src/App.js
@@ -24,14 +24,7 @@ class App extends Component {
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path='/' 
-          element={<Home />}
-          render={() => {
-            return (
-              this.state.isPageLoaded ?
-              <Navigate to="/" /> :
-              <Navigate to="/" />
-    )}} />
+          <Route path='/' exact element={<Home />}/>
           <Route path='/services' element={<Services />} />
           <Route path='/products' element={<Products />} />
           <Route path='/sign-up' element={<SignUp />} />
