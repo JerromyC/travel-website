3e8d4bf9250f4900151ddac81f01136679cae3d5
JerromyC <59486979+JerromyC@users.noreply.github.com>
2022-06-06 16:23:38 -0500

    Attempt: Fix initial load and relaod with working home page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React from 'react'
+import { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Home from './components/pages/Home.js';
 import Products from './components/pages/Products.js';
 import Services from './components/pages/Services.js';
 import SignUp from './components/pages/SignUp.js';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Footer from './components/Footer';
 
 
-function App() {
-  return (
-    <>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/sign-up' element={<SignUp />} />
-      </Routes>
-      <Footer />
-    </Router>
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      isUserAuthenticated: true
+    };
+  }
+
+  render () {
+    return (
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' 
+          element={<Home />}
+          render={() => {
+            return (
+              this.state.isUserAuthenticated ?
+              <Navigate to="/" /> :
+              <Navigate to="/sign-up" />
+    )}} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
